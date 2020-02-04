d04728e124951f89dfed22bb430cc3378e4acbc3
Redouane Bouzekri <bouzekri.redouane@gmail.com>
2020-02-04 04:15:50 +0100

    Commit the project creation

---
+++ picking_numbers/picking_numbers/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  picking_numbers
+//
+//  Created by Redouane Bouzekri on 2/4/20.
+//  Copyright © 2020 Redouane Bouzekri. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
